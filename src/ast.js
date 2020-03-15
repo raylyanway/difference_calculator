eb11926b91a0b8aab462eb3049643f203f399440
iLya Raylyan <IRaylyan@alfabank.ru>
2020-03-15 23:25:15 +0300

    refactor(ast): delete extra return statement

--- src/ast.js
+++ src/ast.js
@@ -3,21 +3,19 @@ import _ from 'lodash';
 const makeAst = (obj1, obj2) => {
   const unitedKeys = _.union(Object.keys(obj1), Object.keys(obj2));
   return unitedKeys.map((key) => {
+    let result = { name: key, type: 'equal', value: obj1[key] };
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return { name: key, type: 'objects', children: makeAst(obj1[key], obj2[key]) };
-    }
-    if (!_.has(obj1, key)) {
-      return { name: key, type: 'added', value: obj2[key] };
-    }
-    if (!_.has(obj2, key)) {
-      return { name: key, type: 'deleted', value: obj1[key] };
-    }
-    if (obj1[key] !== obj2[key]) {
-      return {
+      result = { name: key, type: 'objects', children: makeAst(obj1[key], obj2[key]) };
+    } else if (!_.has(obj1, key)) {
+      result = { name: key, type: 'added', value: obj2[key] };
+    } else if (!_.has(obj2, key)) {
+      result = { name: key, type: 'deleted', value: obj1[key] };
+    } else if (obj1[key] !== obj2[key]) {
+      result = {
         name: key, type: 'updated', value1: obj1[key], value2: obj2[key],
       };
     }
-    return { name: key, type: 'equal', value: obj1[key] };
+    return result;
   });
 };
 
