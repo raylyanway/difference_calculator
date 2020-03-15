d29bde42106e8b54784de4fbf50247ba0f0ba72e
iLya Raylyan <IRaylyan@alfabank.ru>
2020-03-15 23:59:59 +0300

    refactor(tests): rewrite tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,62 +3,19 @@ import gendiff from '../src';
 
 const prefix = '__tests__/__fixtures__/';
 
-test('json files are equal', () => {
-  const expectedData = gendiff(`${prefix}before.json`, `${prefix}after.json`);
-  const comparedData = fs.readFileSync(`${prefix}result`, 'utf8');
-  expect(expectedData).toBe(comparedData);
-});
+[
+  { ext: 'ini' }, { ext: 'yaml' }, { ext: 'json' },
+  { ext: 'json', nested: true }, { ext: 'json', nested: true }, { ext: 'json', nested: true },
+]
+  .forEach(({ ext, nested }) => test(`${ext}${nested ? 'Ast' : ''} files are equal`, () => {
+    const expected = gendiff(`${prefix}before${nested ? 'Nested' : ''}.${ext}`, `${prefix}after${nested ? 'Nested' : ''}.${ext}`);
+    const compared = fs.readFileSync(`${prefix}result${nested ? 'Nested' : ''}`, 'utf8');
+    expect(expected).toBe(compared);
+  }));
 
-test('yaml files are equal', () => {
-  const expectedData = gendiff(`${prefix}before.yaml`, `${prefix}after.yaml`);
-  const comparedData = fs.readFileSync(`${prefix}result`, 'utf8');
-  expect(expectedData).toBe(comparedData);
-});
-
-test('ini files are equal', () => {
-  const expectedData = gendiff(`${prefix}before.ini`, `${prefix}after.ini`);
-  const comparedData = fs.readFileSync(`${prefix}result`, 'utf8');
-  expect(expectedData).toBe(comparedData);
-});
-
-test('jsonAst files are equal', () => {
-  const expectedData = gendiff(`${prefix}beforeNested.json`, `${prefix}afterNested.json`);
-  const comparedNestedData = fs.readFileSync(`${prefix}resultNested`, 'utf8');
-  expect(expectedData).toBe(comparedNestedData);
-});
-
-test('yamlAst files are equal', () => {
-  const expectedData = gendiff(`${prefix}beforeNested.yaml`, `${prefix}afterNested.yaml`);
-  const comparedNestedData = fs.readFileSync(`${prefix}resultNested`, 'utf8');
-  expect(expectedData).toBe(comparedNestedData);
-});
-
-test('iniAst files are equal', () => {
-  const expectedData = gendiff(`${prefix}beforeNested.ini`, `${prefix}afterNested.ini`);
-  const comparedNestedData = fs.readFileSync(`${prefix}resultNested`, 'utf8');
-  expect(expectedData).toBe(comparedNestedData);
-});
-
-test('json files with format Plain are equal', () => {
-  const expectedData = gendiff(`${prefix}beforeNested.json`, `${prefix}afterNested.json`, 'plain');
-  const comparedPlainFormatData = fs.readFileSync(`${prefix}resultPlain`, 'utf8');
-  expect(expectedData).toBe(comparedPlainFormatData);
-});
-
-test('yamlAst files with format Plain are equal', () => {
-  const expectedData = gendiff(`${prefix}beforeNested.yaml`, `${prefix}afterNested.yaml`, 'plain');
-  const comparedPlainFormatData = fs.readFileSync(`${prefix}resultPlain`, 'utf8');
-  expect(expectedData).toBe(comparedPlainFormatData);
-});
-
-test('iniAst files with format Plain are equal', () => {
-  const expectedData = gendiff(`${prefix}beforeNested.ini`, `${prefix}afterNested.ini`, 'plain');
-  const comparedPlainFormatData = fs.readFileSync(`${prefix}resultPlain`, 'utf8');
-  expect(expectedData).toBe(comparedPlainFormatData);
-});
-
-test('json files with format Json are equal', () => {
-  const expectedData = gendiff(`${prefix}beforeNested.json`, `${prefix}afterNested.json`, 'json');
-  const comparedJsonFormatData = fs.readFileSync(`${prefix}resultJson`, 'utf8');
-  expect(expectedData).toBe(comparedJsonFormatData);
-});
+[{ ext: 'ini' }, { ext: 'yaml' }, { ext: 'json' }, { ext: 'json', format: 'Json' }]
+  .forEach(({ ext, format = 'Plain' }) => test(`${ext} files with format ${format} are equal`, () => {
+    const expected = gendiff(`${prefix}beforeNested.${ext}`, `${prefix}afterNested.${ext}`, format.toLowerCase());
+    const compared = fs.readFileSync(`${prefix}result${format}`, 'utf8');
+    expect(expected).toBe(compared);
+  }));
